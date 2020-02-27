92728d62e6ad11a8e7bd335387af35017951e066
nottdev <siriphon@gmail.com>
2020-02-27 23:19:10 +0700

    Add response version

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3011;
+const port = process.env.PORT || 3000;
 const path = require('path');
 
 app.get('/', (req, res) => {
-  res.send({status: 'ok'});
+  res.send({
+    version: '1.0.1',
+    status: 'ok'
+  });
 });
 
 app.listen(port, ()=>console.log(`Server is running on port ${port}`));
